b14fa12044e3d20c9ae04f0edfff1aec6dfa14d6
RISHABH SHARMA <85387033+Rishabh2134@users.noreply.github.com>
2021-08-11 20:15:42 +0530

    This is the basic of split function in Python

---
+++ Splitting.py
@@ -0,0 +1,22 @@
+#split
+
+abc = ('My name is Rishabh')
+f = abc.split()
+print(f[0])
+
+for w in f:
+    print(w)
+
+a = ('I;am;an;Engineer')
+b = a.split()
+print(b)
+b = a.split(';')
+print(b)
+
+c = ('My Email id is rishabh19092001@gmail.com')
+d = c.split()
+print(c)
+e = d[4]
+print(e)
+f = e.split('@')
+print(f[1])
